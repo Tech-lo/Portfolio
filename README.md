7e20087d872da00e1abe5a677357ae3f1d9bb9a6
Tenzin <94886097+Tech-lo@users.noreply.github.com>
2024-04-23 16:27:54 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Portfolio
+# finalportfolio
+Latest Portfolio
+link https://chulbul7061.github.io/finalportfolio/#
---
+++ script.js
@@ -0,0 +1,30 @@
+// Navbar Section
+function showSidebar(){
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'flex';
+}
+
+function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'none';
+}
+// Home Section
+
+var typed = new Typed('#element', {
+  strings: ['SDE', ' Web Developer'],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true
+});
+
+
+
+
+
+
+
+
+
+
+  
+ 
